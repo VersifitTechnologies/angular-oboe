865977ab1f9edf2ea0df6a4f8fb33c59513d3988
Kyle Kemp <kyle@seiyria.com>
2015-03-12 13:56:35 -0500

    make oboe promise-based so data can be intercepted

--- dist/angular-oboe.js
+++ dist/angular-oboe.js
@@ -8,17 +8,7 @@ angular.module('ngOboe', []).service('Oboe', [
     // the pattern contains a path which selects json objects from the stream
     // the pagesize parameter determines how many JSON objects need to be received before the controller gets updated. Defaults to 100.
     return function (params) {
-      // initialize the data
-      var data = [];
-      // add the data page by page using a stream
-      OboeStream.search(params, function (page) {
-        // a page of records is received.
-        // add each record to the data
-        angular.forEach(page, function (record) {
-          data.push(record);
-        });
-      });
-      return data;
+      return OboeStream.search(params);
     };
   }
 ]).factory('OboeStream', [
@@ -28,7 +18,6 @@ angular.module('ngOboe', []).service('Oboe', [
       search: function (params, callback) {
         // the defer will be resolved immediately
         var defer = $q.defer();
-        var promise = defer.promise;
         // counter for the received records
         var counter = 0;
         // I use an arbitrary page size.
@@ -37,24 +26,16 @@ angular.module('ngOboe', []).service('Oboe', [
         var page = [];
         // call the oboe function to start the stream
         oboe(params).start(function () {
-          defer.resolve();
         }).node(params.pattern || '.', function (node) {
-          //  we push the node to the page
-          page.push(node);
-          counter++;
-          // if the pagesize is reached return the page using the promise
-          if (counter % pagesize === 0) {
-            promise.then(callback(page));
-            // initialize the page
-            page = [];
-          }
+          defer.notify(node);
           // freeup memory
           return oboe.drop;
         }).done(function (result) {
           // when the stream is done make sure the last page of nodes is returned
-          promise.then(callback(page));
+          defer.notify(result);
+          defer.resolve();
         });
-        return promise;
+        return defer.promise;
       }
     };
   }
