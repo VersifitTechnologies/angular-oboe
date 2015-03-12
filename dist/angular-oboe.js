856b384ebae612624f4601d319a1848cff1c3732
Kyle Kemp <kyle@seiyria.com>
2015-03-12 14:19:14 -0500

    updates

--- dist/angular-oboe.js
+++ dist/angular-oboe.js
@@ -6,7 +6,6 @@ angular.module('ngOboe', []).service('Oboe', [
     // all parameters consumed by the oboe module can be consumed
     // the url needs to return a json stream. see the oboe documentation
     // the pattern contains a path which selects json objects from the stream
-    // the pagesize parameter determines how many JSON objects need to be received before the controller gets updated. Defaults to 100.
     return function (params) {
       return OboeStream.search(params);
     };
@@ -15,25 +14,18 @@ angular.module('ngOboe', []).service('Oboe', [
   '$q',
   function ($q) {
     return {
-      search: function (params, callback) {
-        // the defer will be resolved immediately
+      search: function (params) {
         var defer = $q.defer();
-        // counter for the received records
-        var counter = 0;
-        // I use an arbitrary page size.
-        var pagesize = params.pagesize || 100;
-        // initialize the page of records
-        var page = [];
         // call the oboe function to start the stream
         oboe(params).start(function () {
         }).node(params.pattern || '.', function (node) {
+          //update the caller
           defer.notify(node);
-          // freeup memory
+          // free up memory
           return oboe.drop;
         }).done(function (result) {
-          // when the stream is done make sure the last page of nodes is returned
-          defer.notify(result);
-          defer.resolve();
+          // when the stream is done, pass this data along
+          defer.resolve(result);
         });
         return defer.promise;
       }
--- src/angular-oboe.js
+++ src/angular-oboe.js
@@ -1,47 +1,40 @@
 'use strict';
-angular.module('ngOboe', [])
-    .service('Oboe', [
-        'OboeStream', 
-        function(OboeStream) {
+angular.module('ngOboe', []).service('Oboe', [
+    'OboeStream',
+    function (OboeStream) {
         // the passed parameters object need to have a Url and a pattern.
         // all parameters consumed by the oboe module can be consumed
         // the url needs to return a json stream. see the oboe documentation
         // the pattern contains a path which selects json objects from the stream
-        // the pagesize parameter determines how many JSON objects need to be received before the controller gets updated. Defaults to 100.
-        return function(params) {
+        return function (params) {
             return OboeStream.search(params);
         };
-    }])
-    .factory('OboeStream', ['$q', function($q) {
+    }
+]).factory('OboeStream', [
+    '$q',
+    function ($q) {
         return {
-            // the search function calls the oboe module to get the JSON data in a stream
-            search: function(params, callback) {
-                // the defer will be resolved immediately
+            search: function (params) {
                 var defer = $q.defer();
-                // counter for the received records
-                var counter = 0;
-                // I use an arbitrary page size.
-                var pagesize = params.pagesize || 100;
-                // initialize the page of records
-                var page = [];
+
                 // call the oboe function to start the stream
-                oboe(params)
-                    // once the stream starts we can resolve the defer.
-                    .start(function() {
-                    })
-                    // for every node containing the specified pattern or if not specified any node
-                    .node(params.pattern || '.', function(node) {
-                        defer.notify(node);
-                        // freeup memory
-                        return oboe.drop;
-                    })
-                    .done(function(result) {
-                        // when the stream is done make sure the last page of nodes is returned
-                        defer.notify(result);
-                        defer.resolve();
-                    });
+                oboe(params).start(function () {
+                	// do something?
+                }).node(params.pattern || '.', function (node) {
+
+                	//update the caller
+                    defer.notify(node);
+
+                    // free up memory
+                    return oboe.drop;
+                }).done(function (result) {
+
+                    // when the stream is done, pass this data along
+                    defer.resolve(result);
+                });
 
                 return defer.promise;
             }
         };
-    }]);
+    }
+]);
